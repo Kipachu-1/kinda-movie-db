10304e1462735c30482b1f9a95469dd281c07207
Kipachu <arsik7473838@gmail.com>
2024-11-07 11:19:31 +0500

    Initial Commit

---
+++ kinda-movie-dbUITests/kinda_movie_dbUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  kinda_movie_dbUITestsLaunchTests.swift
+//  kinda-movie-dbUITests
+//
+//  Created by Arsen Kipachu on 11/7/24.
+//
+
+import XCTest
+
+final class kinda_movie_dbUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
